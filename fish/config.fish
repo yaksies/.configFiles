cfb627eb51382c623165807458c25c555abf99df
yaksies <samyakghising@gmail.com>
2025-01-10 18:43:05 -0500

    My ~./config files for MacOS all at once for the first time.

---
+++ fish/config.fish
@@ -0,0 +1,5 @@
+starship init fish | source
+
+zoxide init fish | source
+
+source /Users/imac/.docker/init-fish.sh || true # Added by Docker Desktop
---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,12 @@
+return {
+  -- animations
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.scroll = {
+        enable = false,
+      }
+    end,
+  },
+}
